130a6c74d2262d2d2729b8439674be4eb06cbe75
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2024-04-09 16:59:14 +0200

    shock + screen + led

--- index/index.ino
+++ index/index.ino
@@ -1,50 +1,45 @@
-int Led = 25;    // LED on Arduino board
-int Shock = 32;  // sensor signal
-int buzzer = 23;
-int val;  // numeric variable to store sensor status
-
+int Led = 26;    // LED on Arduino board
+int Shock = 21;  // sensor signal
+int val;         // numeric variable to store sensor status
+unsigned long lastUpdateTime = 0;
+unsigned long updateInterval = 100; // Interval to update the shock sensor, in milliseconds
+ 
+#include "SevSeg.h"
+SevSeg sevseg;
+ 
 void setup() {
-  pinMode(Led, OUTPUT);     // define LED as output interface
-  pinMode(Shock, INPUT);    // define input for sensor signal
-  pinMode(buzzer, OUTPUT);  // set pin 8 as output
+  pinMode(Led, OUTPUT);   // define LED as output interface
+  pinMode(Shock, INPUT);  // define input for sensor signal
   Serial.begin(9600);
+ 
+  byte numDigits = 4;
+  byte digitPins[] = { 32, 13, 14, 15 };
+  byte segmentPins[] = { 33, 25, 22, 19, 18, 12, 23, 5 };
+  bool updateWithDelays = false;
+  sevseg.begin(COMMON_ANODE, numDigits, digitPins, segmentPins, updateWithDelays);
+  sevseg.setBrightness(100);
 }
-
+ 
 void loop() {
-
-
+  // Check if it's time to update the shock sensor
+  if (millis() - lastUpdateTime >= updateInterval) {
+    lastUpdateTime = millis(); // Update the last update time
+ 
+    shock(); // appel de la fonction shock pour vérifier le capteur de vibration
+  }
+ 
+  sevseg.setNumber(1234); // Mise à jour de l'affichage directement ici
+  sevseg.refreshDisplay(); // Mettre à jour l'affichage
+}
+ 
+void shock() {
   val = digitalRead(Shock);  // read and assign the value of digital interface 3 to val
   if (val == HIGH)           // when sensor detects a signal, the LED flashes
   {
     digitalWrite(Led, HIGH);
-    unsigned char i;
-    while (1) {
-      // In this program, the buzzer is controlled alternately with two different frequencies.
-      // The signal consists of a square wave voltage.
-      // Turning the buzzer on and off will generate a tone that roughly corresponds to the frequency.
-      // The frequency is defined by the length of the on and off phase.
-
-      //Frequency 1
-      for (i = 0; i < 80; i++) {
-        digitalWrite(buzzer, HIGH);
-        delay(1);
-        digitalWrite(buzzer, LOW);
-        delay(1);
-      }
-      //Frequency 2
-      for (i = 0; i < 100; i++) {
-        digitalWrite(buzzer, HIGH);
-        delay(2);
-        digitalWrite(buzzer, LOW);
-        delay(2);
-      }
-    }
     Serial.println("Im high");
-    delay(500);
   } else {
     digitalWrite(Led, LOW);
     Serial.println("Im low");
-    delay(500);
   }
-  delay(1000);
 }
