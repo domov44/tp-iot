c1f609c421778e0f667404d6576638b7967618e9
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2024-04-09 11:49:50 +0200

    [ADD] first test with ky-031 success

--- index/index.ino
+++ index/index.ino
@@ -1,10 +1,25 @@
-void setup() {
-  pinMode(25, OUTPUT);
+int Led = 25;	// LED on Arduino board 
+int Shock = 32;	// sensor signal
+int val;		// numeric variable to store sensor status
+
+void setup()
+{
+	pinMode(Led, OUTPUT); 	// define LED as output interface
+	pinMode(Shock, INPUT); 	// define input for sensor signal
+  Serial.begin(9600);
 }
 
-void loop() {
-  digitalWrite(25, HIGH);
-  delay(1000);
-  digitalWrite(25, LOW);
-  delay(500);
+void loop()
+{
+	val = digitalRead(Shock); // read and assign the value of digital interface 3 to val
+	if(val == HIGH) // when sensor detects a signal, the LED flashes
+	{
+		digitalWrite(Led, HIGH);
+    Serial.println("Im high");
+	}
+	else
+	{
+		digitalWrite(Led, LOW);
+    Serial.println("Im low");
+	}
 }
